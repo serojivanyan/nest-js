97fc8da83b4f6f08b612695a02d128387ad2dae1
Seryozha <seroj.ivanyan@gmail.com>
2021-10-08 17:35:13 +0400

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  console.log(process.env.PORT);
+  console.log(process.env.MODE);
+  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port, () => console.log(`Server listen on port ${port}`));
+}
+bootstrap();
